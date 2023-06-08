e3399c17679d56f9fe20874ecf8afeca7aa500b1
unknown <z5292875@ad.unsw.edu.au>
2023-06-08 18:19:38 +1000

    fix bug in google login api and sanity database

--- shareme_frontend/src/component/Login.jsx
+++ shareme_frontend/src/component/Login.jsx
@@ -12,14 +12,15 @@ import { client } from '../client';
 
 
 const Login = () => {
-  const clientID = '257306832534-u4rr6eis21h2sn1pmpmem4ts6r3o55j7.apps.googleusercontent.com'
+  
   
   const navigate = useNavigate();
   const responseGoogle = (response) => {
 
-    localStorage.setItem('user', JSON.stringify(response.credential));
+    // localStorage.setItem('user', JSON.stringify(response.credential));
+    
     let res = localStorage.getItem
-    console.log(res)
+    console.log('Res:',res)
     let decodedHeader = jwt_decode(response.credential)
     console.log(decodedHeader)
     const { name, sub, picture } = decodedHeader ;
@@ -30,15 +31,18 @@ const Login = () => {
       userName: name,
       image: picture,
     };
-    
+
+    console.log('doc:'+doc._id.type)
     console.log('doc:'+doc._id)
     console.log('doc:'+doc.userName)
     console.log('doc:'+doc.image)
+
 
     client.createIfNotExists(doc)
       .then(() => {
         navigate('/', { replace: true });
     });
+    // navigate('/', { replace: true });
     
     
     
@@ -67,7 +71,7 @@ const Login = () => {
           <div className="shadow-2xl">
             
 {/* one correct version */}
-            <GoogleOAuthProvider clientId= {clientID}>
+            <GoogleOAuthProvider clientId= {process.env.REACT_APP_GOOGLE_API_TOKEN}>
               <GoogleLogin
                 
                 onSuccess={responseGoogle}
@@ -77,37 +81,6 @@ const Login = () => {
               />
             </GoogleOAuthProvider>
 
-
-              {/* <GoogleLogin
-                clientId={clientID}
-                buttonText='Welcome to login with Google'
-                onSuccess={responseGoogle}
-                onFailure={(fail)=>{
-                  console.log('Login Failed');
-                  console.log(fail);
-                }}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={true}
-              /> */}
-            
-              
-            
-            {/* <GoogleLogin
-              
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="mr-4" /> Sign in with google
-                </button>
-              )}
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy="single_host_origin"
-            /> */}
           </div>
         </div>
       </div>
